7a4729f1a1695b638665c62ad4f864f792e9fe40
Shariar Ahmed <najmulhasanshahin@gmail.com>
2025-10-22 02:20:22 +0600

    Auto-deploy updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#16a34a",
+        secondary: "#22c55e",
+        bg: "#f1f5f9",
+      },
+    },
   },
-  plugins: [require("daisyui")],
-};
+  plugins: [],
+}
